184fe9647d30fe184811699bdd68899f2661b1c6
SDHaines <scott.d.haines@gmail.com>
2021-05-03 16:07:08 -0400

    Initial commit

---
+++ priceChangesSalesFloor.php
@@ -0,0 +1,39 @@
+#!/usr/local/bin/php
+<?php
+
+/* 
+ * This script queries the database and outputs the 
+ * item and new price for items that reside on 
+ * the sales floor. It's runs from cron on Friday 
+ * afternoon at 3:00 to allow for all current price 
+ * changes to be entered into the database. The 
+ * output is directed to shared storage so that
+ * the managers can print new price tickets. 
+ */
+
+require_once '/usr/local/valCommon/Counterpoint.php';
+
+$startDT = date('Y/m/d h:i:s', strtotime('saturday last week'));
+$tsql = "SELECT
+P.ITEM_NO,
+I.BARCOD,
+I.ADDL_DESCR_1,
+P.PRC_1, 
+P.LST_MAINT_DT,
+P.LST_MAINT_USR_ID
+FROM dbo.IM_PRC P
+LEFT JOIN IM_ITEM I ON P.ITEM_NO = I.ITEM_NO
+WHERE  (I.ADDL_DESCR_1 NOT LIKE '% 50[mM][lL]%' OR I.ADDL_DESCR_1 NOT LIKE '% 375[mM][lL]%')
+AND P.LST_MAINT_DT >= '$startDT'
+ORDER BY P.LST_MAINT_DT DESC";
+
+print "item,price\n";
+$result = counterpointQuickQuery( $tsql, "callback" ) ;
+if ( $result === false ){
+  die("something went wrong ^^^ those error messages should point you in the right direction\n" );
+}
+
+function callback( $row ) { 
+    printf("%s,%.02f\n", $row['ADDL_DESCR_1'],$row['PRC_1'] );
+}
+?>
